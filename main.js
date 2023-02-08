3cb4f73ddde004aa39b494d568b72efd848c086f
AmongusPerson <95143897+AmongusPerson@users.noreply.github.com>
2023-02-08 17:59:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload() {
+    mariodie = loadSound("mariodie.wav");
+    gameover = loadSound("gameover.wav");
+    jump = loadSound("jump.wav");
+    world_start = loadSound("world_start.wav");
+    kick = loadSound("kick.wav");
+    coin = loadSound("coin.wav");
+    setSprite();
+    marioAnimation();
+}
